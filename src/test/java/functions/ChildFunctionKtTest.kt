95c03eaf81f3bd048315a1cfcbfcaa487e39d3bf
Juan Pablo <juan.pa097@gmail.com>
2020-03-01 16:50:09 +0000

    Created tests for child, node and sibling function.

---
+++ src/test/java/functions/ChildFunctionKtTest.kt
@@ -0,0 +1,48 @@
+package functions
+
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import tree.TargetTreeNode
+
+internal class ChildFunctionKtTest {
+
+    @Mock
+    private lateinit var mockTargetTreeNode: TargetTreeNode
+
+    @Mock
+    private lateinit var mockCurrentTargetTreeNode: TargetTreeNode
+
+    private lateinit var mockFatherVisitorFunction: VisitorFunction
+    private lateinit var mockChildVisitorFunction: VisitorFunction
+
+    @BeforeEach
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        mockFatherVisitorFunction = { listOf(mockTargetTreeNode) }
+        mockChildVisitorFunction = { listOf(mockTargetTreeNode) }
+    }
+
+    @Test
+    fun `should move down first and then up`() { // I think I need some help naming this test according to context.
+        // arrange
+        val children = listOf(mockCurrentTargetTreeNode, mockCurrentTargetTreeNode)
+        val function = childFunction(mockFatherVisitorFunction, mockChildVisitorFunction)
+        `when`(mockCurrentTargetTreeNode.moveUp()).thenReturn(mockCurrentTargetTreeNode)
+        `when`(mockCurrentTargetTreeNode.moveDown()).thenReturn(mockCurrentTargetTreeNode)
+        // act
+        val result = function(mockCurrentTargetTreeNode)
+        // assert
+        verify(mockCurrentTargetTreeNode, times(1)).moveUp()
+        verify(mockCurrentTargetTreeNode, times(0)).moveRight()
+        verify(mockCurrentTargetTreeNode, times(0)).moveLeft()
+        verify(mockCurrentTargetTreeNode, times(1)).moveDown()
+
+        assertEquals(children.size, result.size)
+    }
+}
