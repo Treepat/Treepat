5924acbc6cefd0b183ba12c808a3fd872c44dbcc
Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
2020-03-05 11:04:33 -0500

    Fix bug in fibling function (#21)
    * fix the bug in sibling function

--- src/main/java/functions/SiblingFunction.kt
+++ src/main/java/functions/SiblingFunction.kt
@@ -4,7 +4,11 @@ fun siblingFunction(
     siblings: List<VisitorFunction>
 ): VisitorFunction {
     return { targetTreeNode ->
-        val answer = siblings.map { it.invoke(targetTreeNode) }
+        var currentNode = targetTreeNode
+        val answer = siblings.mapIndexed { index, function ->
+            if (index != 0) { currentNode = currentNode.moveRight() }
+            function.invoke(currentNode)
+        }
         val isAnyListEmpty = answer.any { it.isEmpty() }
         if (isAnyListEmpty) {
             listOf()
