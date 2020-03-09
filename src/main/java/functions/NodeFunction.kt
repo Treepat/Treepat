0045601c87a0a26fea8e759353962423f64bbcbf
Johan Murillo <johan_smc@hotmail.com>
2020-03-09 17:06:45 -0500

    reduce node function code

--- src/main/java/functions/NodeFunction.kt
+++ src/main/java/functions/NodeFunction.kt
@@ -2,11 +2,9 @@ package functions
 
 fun nodeFunction(grammarNodeName: String): VisitorFunction {
     return { targetTreeNode ->
-        targetTreeNode?.let {
-            when (it.name) {
-                grammarNodeName -> listOf(it)
-                else -> listOf()
-            }
-        } ?: listOf()
+        when (targetTreeNode?.name) {
+            grammarNodeName -> listOf(targetTreeNode)
+            else -> listOf()
+        }
     }
 }
