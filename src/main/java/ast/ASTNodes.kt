bdd3a0826a194a1de4494b7705fd746666226ae6
Johan Murillo <johan_smc@hotmail.com>
2020-03-10 13:55:36 -0500

    make string constants

--- src/main/java/ast/ASTNodes.kt
+++ src/main/java/ast/ASTNodes.kt
@@ -2,12 +2,15 @@ package ast
 
 interface ASTNode
 
+const val indentString = "    "
+const val endLineString = "\n"
+
 data class Child(var father: ASTNode, var child: ASTNode) : ASTNode {
-    override fun toString(): String = "$father\n${child.toString().prependIndent("    ")}"
+    override fun toString(): String = "$father\n${child.toString().prependIndent(indentString)}"
 }
 data class Node(var name: String) : ASTNode {
     override fun toString(): String = name
 }
 data class Sibling(var siblings: List<ASTNode>) : ASTNode {
-    override fun toString(): String = siblings.joinToString(separator = "\n")
+    override fun toString(): String = siblings.joinToString(separator = endLineString)
 }
