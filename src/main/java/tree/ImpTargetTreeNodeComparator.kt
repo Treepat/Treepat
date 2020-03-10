cf4ece32f1b1d764d9b0f1d03e20e22fd1031489
DorianMoreno <dorian.r.mv@gmail.com>
2020-03-10 11:44:52 -0500

    Update src/main/java/tree/ImpTargetTreeNodeComparator.kt
    Co-Authored-By: Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>

--- src/main/java/tree/ImpTargetTreeNodeComparator.kt
+++ src/main/java/tree/ImpTargetTreeNodeComparator.kt
@@ -1,7 +1,7 @@
 package tree
 
 class ImpTargetTreeNodeComparator : Comparator<ImpTargetTreeNode> {
-    override fun compare(o1: ImpTargetTreeNode?, o2: ImpTargetTreeNode?): Int {
+    override fun compare(node1: ImpTargetTreeNode?, node2: ImpTargetTreeNode?): Int {
         if (o1 == null || o2 == null)
             return 0
         return o1!!.getId().compareTo(o2!!.getId())
