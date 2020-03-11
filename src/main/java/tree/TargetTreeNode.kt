17c080ae79077795eff1249f02cf35dc8dedefe7
DorianMoreno <dorian.r.mv@gmail.com>
2020-03-11 16:58:13 -0500

    Tree file parsing (#28)
    * Refactoring ANTLR files
    
    * Organizing files
    
    * Delete misc.xml
    
    * standarize package names
    
    * Refactor and working parser
    
    * working implementation
    
    * visitor refactoring
    
    * Refactor TargetTreeNode interface
    
    * PR requested changes
    
    * ktlint formatting
    
    * Null Safety fix
    
    Co-Authored-By: Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
    
    * PR requested changes #2
    
    * Update src/main/java/tree/ImpTargetTreeNodeComparator.kt
    
    Co-Authored-By: Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
    
    * Last PR requests solved
    
    Co-authored-by: Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
    Co-authored-by: Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>

--- src/main/java/tree/TargetTreeNode.kt
+++ src/main/java/tree/TargetTreeNode.kt
@@ -1,9 +1,15 @@
 package tree
 
-interface TargetTreeNode : Comparable<TargetTreeNode> {
-    fun moveRight(): TargetTreeNode
-    fun moveLeft(): TargetTreeNode
-    fun moveUp(): TargetTreeNode
-    fun moveDown(): TargetTreeNode
+interface TargetTreeNode {
     val name: String
+    val tag: String
+    val id: Int
+    var children: List<TargetTreeNode>
+
+    fun moveToRightSibling(): TargetTreeNode?
+    fun moveToLeftSibling(): TargetTreeNode?
+    fun moveToParent(): TargetTreeNode?
+    fun moveToFirstChild(): TargetTreeNode?
+
+    override fun toString(): String
 }
