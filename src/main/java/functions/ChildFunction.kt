7666bbb6a159f5e0b9b447e85d528952866f6f6e
Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
2020-03-11 18:26:18 -0500

    Catch null nodes in high-level functions (#31)
    * Catch null nodes in high-level functions
    
    * reduce node function code

--- src/main/java/functions/ChildFunction.kt
+++ src/main/java/functions/ChildFunction.kt
@@ -6,9 +6,9 @@ fun childFunction(
 ): VisitorFunction {
     return { targetTreeNode ->
         val fathers = father.invoke(targetTreeNode)
-        val children = targetTreeNode.moveToFirstChild()?.let { child.invoke(it) }
+        val children = child.invoke(targetTreeNode?.moveToFirstChild())
         when {
-            fathers.isEmpty() || children!!.isEmpty() -> listOf()
+            fathers.isEmpty() || children.isEmpty() -> listOf()
             else -> listOf(fathers, children).flatten()
         }
     }
