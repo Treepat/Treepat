958b3b68499f1ebb53f0ee365d34c0e9e925aa46
Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
2020-03-11 18:26:48 -0500

    Create a new format for testing (#29)
    * add to string in data clases and change test cases
    
    * add test cases in files and change test functions
    
    * remove words
    
    * Update src/test/java/ast/mix/2.msg
    
    Co-Authored-By: DorianMoreno <dorian.r.mv@gmail.com>
    
    * make string constants
    
    * mayus const strings
    
    Co-authored-by: DorianMoreno <dorian.r.mv@gmail.com>

--- src/main/java/ast/ASTNodes.kt
+++ src/main/java/ast/ASTNodes.kt
@@ -2,6 +2,15 @@ package ast
 
 interface ASTNode
 
-data class Child(var father: ASTNode, var child: ASTNode) : ASTNode
-data class Node(var name: String) : ASTNode
-data class Sibling(var siblings: List<ASTNode>) : ASTNode
+const val INDENT_STRING = "    "
+const val END_LINE_STRING = "\n"
+
+data class Child(var father: ASTNode, var child: ASTNode) : ASTNode {
+    override fun toString(): String = "$father\n${child.toString().prependIndent(INDENT_STRING)}"
+}
+data class Node(var name: String) : ASTNode {
+    override fun toString(): String = name
+}
+data class Sibling(var siblings: List<ASTNode>) : ASTNode {
+    override fun toString(): String = siblings.joinToString(separator = END_LINE_STRING)
+}
