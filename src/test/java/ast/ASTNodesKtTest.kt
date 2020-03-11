adb1ef71e2bc76ef8eec3c4b2c8da9e16ff9f5fc
Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
2020-03-11 18:26:48 -0500

    Create a new format for testing (#29)
    * add to string in data clases and change test cases
    
    * add test cases in files and change test functions
    
    * remove words
    
    * Update src/test/java/ast/mix/2.msg
    
    Co-Authored-By: DorianMoreno <dorian.r.mv@gmail.com>
    
    * make string constants
    
    * mayus const strings
    
    Co-authored-by: DorianMoreno <dorian.r.mv@gmail.com>

--- src/test/java/ast/ASTNodesKtTest.kt
+++ src/test/java/ast/ASTNodesKtTest.kt
@@ -3,26 +3,14 @@ package ast
 import antlr.treepatParser.TreepatVisitorImplementation
 import antlr.treepatParser.generated.TreepatLexer
 import antlr.treepatParser.generated.TreepatParser
+import java.io.File
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTree
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class ASTNodesKtTest {
-
-    private var countNode: Int = 0
-    private var countChild: Int = 0
-    private var countSibling: Int = 0
-
-    @BeforeEach
-    fun setUp() {
-        countNode = 0
-        countChild = 0
-        countSibling = 0
-    }
 
     private fun createParserTree(inputAntlrString: String): ParseTree {
         val lexer = TreepatLexer(CharStreams.fromString(inputAntlrString))
@@ -31,131 +19,79 @@ internal class ASTNodesKtTest {
         return parser.subtree()
     }
 
-    private fun countNodes(node: ASTNode) {
-        when (node) {
-            is Node -> countNode++
-            is Child -> {
-                countNodes(node.father)
-                countNodes(node.child)
-                countChild++
+    private fun getOutputExpected(pathToOutputFile: String): String? {
+        val fileOutput = File(pathToOutputFile)
+        if (fileOutput.exists())
+            return CharStreams.fromFileName(pathToOutputFile).toString()
+        return null
+    }
+
+    private fun runAllTestInFolder(folderPath: String) {
+        File(folderPath).list { _, name -> name.endsWith(".in") }!!.forEach {
+            // arrange
+
+            val inputAntlrString = CharStreams.fromFileName(folderPath + it).toString()
+
+            var output = inputAntlrString
+            getOutputExpected(folderPath + it.replace(".in", ".out"))?.let {
+                outputResult -> output = outputResult
             }
-            is Sibling -> {
-                node.siblings.forEach { countNodes(it) }
-                countSibling++
-            }
-            else -> throw IllegalArgumentException("This ASTNode subtype is not supported.")
+
+            val fileMessage = File(folderPath + it.replace(".in", ".msg")).readLines()
+            val error = fileMessage.first()
+
+            val tree = createParserTree(inputAntlrString)
+            val visitor = TreepatVisitorImplementation()
+
+            // act
+            val root = visitor.visit(tree)
+
+            // assert
+            assertEquals(root.toString(), output, error)
         }
     }
 
     @Test
-    fun `should have one node and then up`() {
-        // arrange
-        val inputAntlrString = """
-            A
-        """.trimIndent()
-        val tree = createParserTree(inputAntlrString)
-        val visitor = TreepatVisitorImplementation()
-        val root = visitor.visit(tree)
-        // act
-        countNodes(root)
-        // assert
-        assertEquals(countNode, 1)
-        assertEquals(countChild, 0)
-        assertEquals(countSibling, 0)
+    fun `should run all node test cases`() {
+        val nodeTestCasesFolder = "./src/test/java/ast/node/"
+        runAllTestInFolder(nodeTestCasesFolder)
     }
 
     @Test
-    fun `should have one child and then up`() {
-        // arrange
-        val inputAntlrString = """
-            A
-                B
-        """.trimIndent()
-        val tree = createParserTree(inputAntlrString)
-        val visitor = TreepatVisitorImplementation()
-        val root = visitor.visit(tree)
-        // act
-        countNodes(root)
-        // assert
-        assertEquals(countNode, 2)
-        assertEquals(countChild, 1)
+    fun `should run all child test cases`() {
+        val nodeTestCasesFolder = "./src/test/java/ast/child/"
+        runAllTestInFolder(nodeTestCasesFolder)
+    }
+    @Test
+    fun `should run all sibling test cases`() {
+        val nodeTestCasesFolder = "./src/test/java/ast/sibling/"
+        runAllTestInFolder(nodeTestCasesFolder)
+    }
+    @Test
+    fun `should run all mix test cases`() {
+        val nodeTestCasesFolder = "./src/test/java/ast/mix/"
+        runAllTestInFolder(nodeTestCasesFolder)
     }
 
     @Test
-    fun `should have at least one sibling and then up`() {
+    fun `should have at least one sibling not end line`() {
+        // Old version
         // arrange
         val inputAntlrString = """
+            A
+                B C
+        """.trimIndent()
+        val output = """
             A
                 B
                 C
         """.trimIndent()
         val tree = createParserTree(inputAntlrString)
         val visitor = TreepatVisitorImplementation()
-        val root = visitor.visit(tree)
         // act
-        countNodes(root)
+        val root = visitor.visit(tree)
         // assert
-        assertEquals(countNode, 3)
+        assertEquals(root.toString(), output)
         // TODO - sibling should have only one node
-        assertTrue(countSibling >= 1)
-    }
-
-    @Test
-    fun `should have many node, three children, two sibling and then up`() {
-        // arrange
-        val inputAntlrString = """
-            A
-                B
-                    C
-                D
-                E
-                    F
-                    G
-        """.trimIndent()
-        val tree = createParserTree(inputAntlrString)
-        val visitor = TreepatVisitorImplementation()
-        val root = visitor.visit(tree)
-        // act
-        countNodes(root)
-        // assert
-        assertEquals(countNode, 7)
-        assertEquals(countChild, 3)
-        // TODO - sibling should have only one node
-        assertTrue(countSibling >= 2)
-    }
-
-    @Test
-    fun `should have one child not tab only one space`() {
-        // arrange
-        val inputAntlrString = """
-            A
-             B
-        """.trimIndent()
-        val tree = createParserTree(inputAntlrString)
-        val visitor = TreepatVisitorImplementation()
-        val root = visitor.visit(tree)
-        // act
-        countNodes(root)
-        // assert
-        assertEquals(countNode, 2)
-        assertEquals(countChild, 1)
-    }
-
-    @Test
-    fun `should have at least one sibling not end line`() {
-        // arrange
-        val inputAntlrString = """
-            A
-                B C
-        """.trimIndent()
-        val tree = createParserTree(inputAntlrString)
-        val visitor = TreepatVisitorImplementation()
-        val root = visitor.visit(tree)
-        // act
-        countNodes(root)
-        // assert
-        assertEquals(countNode, 3)
-        // TODO - sibling should have only one node
-        assertTrue(countSibling >= 1)
     }
 }
