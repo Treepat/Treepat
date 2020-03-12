6973d2f497b629f212950aef22164e7faff108be
Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
2020-03-11 21:40:08 -0500

    Delete generate files (#34)
    * rename packages and delete generate antlr files
    
    * add maven generated files and ignore that

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,9 +1,5 @@
-import antlr.treeFormatParser.TreeFormatVisitorImplementation
-import antlr.treeFormatParser.generated.TreeFormatLexer
-import antlr.treeFormatParser.generated.TreeFormatParser
-import antlr.treepatParser.TreepatVisitorImplementation
-import antlr.treepatParser.generated.TreepatLexer
-import antlr.treepatParser.generated.TreepatParser
+import antlr.tree_format.TreeFormatVisitorImplementation
+import antlr.treepat.TreepatVisitorImplementation
 import ast.ASTNode
 import functions.createVisitorFunction
 import javax.swing.JFrame
@@ -14,6 +10,10 @@ import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.Parser
 import org.antlr.v4.runtime.tree.ParseTree
 import tree.TargetTreeNode
+import tree_format.TreeFormatLexer
+import tree_format.TreeFormatParser
+import treepat.TreepatLexer
+import treepat.TreepatParser
 
 object Main {
 
