dc1e3259a0080409454761b50c545f9b438ebbd8
Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
2020-03-22 12:31:24 -0500

    Create new function and change return VisitorFunction (#52)
    * add new function and change return Visitor Functions
    
    * add test cases
    
    * fix bugs: change order operation
    
    * improve function merge
    
    * fix check function bug
    
    * fix more bugs

--- src/main/java/functions/VisitorFunction.kt
+++ src/main/java/functions/VisitorFunction.kt
@@ -2,13 +2,17 @@ package functions
 
 import ast.ASTNode
 import ast.BreadthClosure
+import ast.Check
 import ast.Child
 import ast.Node
 import ast.Sibling
 import ast.Union
 import tree.TargetTreeNode
 
-data class VisitorFunctionResponse(val matches: List<TargetTreeNode> = listOf(), val hasMatch: Boolean = false)
+data class VisitorFunctionResponse(
+    val matches: List<List<TargetTreeNode>> = listOf(listOf()),
+    val hasMatch: Boolean = false
+)
 
 typealias VisitorFunction = (TargetTreeNode?) -> VisitorFunctionResponse
 
@@ -19,6 +23,21 @@ fun createVisitorFunction(node: ASTNode): VisitorFunction {
         is Sibling -> siblingFunction(node.siblings.map { createVisitorFunction(it) })
         is BreadthClosure -> breadthClosureFunction(createVisitorFunction(node.expression))
         is Union -> unionFunction(node.expressions.map(::createVisitorFunction))
+        is Check -> checkFunction(createVisitorFunction(node.expression))
         else -> throw IllegalArgumentException("This ASTNode subtype is not supported.")
     }
+}
+
+fun mergeList(list: List<List<List<TargetTreeNode>>>): List<List<TargetTreeNode>> {
+    if (list.size <= 1) {
+        return list.first()
+    }
+    val mergeList = mergeList(list.subList(1, list.size))
+    val answer = mutableListOf<List<TargetTreeNode>>()
+    for (firstList in list.first()) {
+        for (secondList in mergeList) {
+            answer.add(firstList + secondList)
+        }
+    }
+    return answer.toList()
 }
