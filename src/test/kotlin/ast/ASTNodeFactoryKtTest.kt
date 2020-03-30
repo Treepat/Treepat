68d6154d295ff5da806f71228199d042a585fa9c
Juan Pablo <juan.pa097@gmail.com>
2020-03-30 16:28:06 -0500

    created test file for ast

---
+++ src/test/kotlin/ast/ASTNodeFactoryKtTest.kt
@@ -0,0 +1,16 @@
+package ast
+
+import org.junit.jupiter.api.Test
+
+internal class ASTNodeFactoryKtTest {
+
+    @Test
+    fun `createSiblingNodes should pair in Siblings a list of nodes`() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+}
