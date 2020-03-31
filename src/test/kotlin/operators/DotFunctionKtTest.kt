32a16f6bf2796b4f39c786edfb442c45b5df80fc
Juan Pablo <juan.pa097@gmail.com>
2020-03-31 12:37:35 -0500

    created test for dot.

--- src/test/kotlin/operators/DotFunctionKtTest.kt
+++ src/test/kotlin/operators/DotFunctionKtTest.kt
@@ -1,7 +1,5 @@
 package operators
 
-import MockValues.tNodeName
-import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Test
@@ -10,19 +8,18 @@ import org.junit.jupiter.api.extension.ExtendWith
 import target_tree.TargetTreeNode
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNull
 
 @ExtendWith(MockKExtension::class)
-internal class NodeFunctionKtTest {
+internal class DotFunctionKtTest {
 
     @MockK
     private lateinit var mockTargetTreeNode: TargetTreeNode
 
     @Test
-    fun `nodeFunction should return a response with match if target node name matches with grammar node`() {
-        // arrange
-        every { mockTargetTreeNode.name } returns tNodeName
+    fun `dotFunction should return matches with any targetTreeNode except null`() {
         // act
-        val result = nodeFunction(tNodeName).invoke(mockTargetTreeNode)
+        val result = dotFunction().invoke(mockTargetTreeNode)
         // assert
         assert(result.hasMatch)
         assertEquals(result.responses.size, 1)
@@ -31,16 +28,13 @@ internal class NodeFunctionKtTest {
     }
 
     @Test
-    fun `nodeFunction should return zero response when match if target node name doesn't matches with grammar node`() {
-        // arrange
-        val tOtherName = "other name"
-        every { mockTargetTreeNode.name } returns tNodeName
+    fun `dotFunction should return zero matches whrn targetTreeNode is null`() {
         // act
-        val result = nodeFunction(tOtherName).invoke(mockTargetTreeNode)
+        val result = dotFunction().invoke(null)
         // assert
         assertFalse(result.hasMatch)
         assertEquals(result.responses.size, 1)
         assertFalse(result.responses.first().matches.contains(mockTargetTreeNode))
-        assertEquals(mockTargetTreeNode, result.responses.first().lastVisitedSibling)
+        assertNull(result.responses.first().lastVisitedSibling)
     }
 }
